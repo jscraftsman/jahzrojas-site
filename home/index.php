9c3ac7c93ae8a24728d4fbe6a49e5dad7a1a526e
Gervel Giva <gervelgiva@gmail.com>
2013-01-14 01:47:28 +0800

    added features and aboutme:incomplete

---
+++ home/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <title>Jahz Rojas | Social Media Design Guru</title>
+        <link rel="stylesheet" href="../css/home.css" type="text/css" media="all" />
+        <script type="text/javascript" src="../js/jquery-1.8.0.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+				
+            });
+        </script>
+    </head>
+    <body>
+        <div class="wrapper">
+			<div id="header">
+				<ul>
+					<li><a href="#" class="header-link" id="service">SERVICES</a></li>
+					<li><a href="#" class="header-link" id="folio">FOLIO</a></li>
+					<li><a href="/jahzrojas/home"><img src="../img/logo.png" id="logo" /></a></li>
+					<li><a href="#" class="header-link" id="blog">BLOG</a></li>
+					<li><a href="#" class="header-link" id="contact">CONTACT</a></li>
+				</ul>
+			</div>
+			<div id="content">
+				<div id="home-body"></div>
+				<div class="separator-1"></div>
+				<div>
+					<p id="quote"> "If you can’t excel with talent, triumph with effort."</p>
+					<p id="author">-Dave Weinbaum</p>
+				</div>
+				<div class="separator-1"></div>
+				<div id="features">
+					<p><img src="../img/featured.png"/></p>
+					<p><a href="#">see more...</a></p>
+				</div>
+				<div id="about-me">
+					<div id="info">
+						<p>A LITTLE ABOUT ME:</p>
+						<p>"One of the greatest things about being a
+							designer with an engineering background
+							is that not only can I take part of the 
+							aesthetical aspect of a project, but can 
+							also fully understand and take care of the 
+							highly technical aspects of it."</p>
+						<img src="../img/jahz-image.png"/>
+					</div>
+				</div>
+			</div>
+			
+        </div>
+    </body>
+</html>
