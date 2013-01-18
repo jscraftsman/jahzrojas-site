fa1c0f5ebbe9d737dd9fb6b4939274d8a30785ce
Gervel Giva <gervelgiva@gmail.com>
2013-01-19 01:22:58 +0800

    site layout ready -links needs correct path

--- home/index.php
+++ home/index.php
@@ -1,3 +1,17 @@
+<?php
+	$testimony1 = "Jahz, was very quick and skilled in creating our company social pages. Really great work! Would definitely recommend her to anyone who is looking to get that handled by a professional.";
+	$author1 = "~ Nichlas Hansen";
+	$website1 = "www.MagnumSalesVideos.com";
+	
+	$testimony2 = "The best graphic designer ever. I will use her again in the future.";
+	$author2 = "~ Christopher Sylvia";
+	$website2 = "www.ExADF.com.au";
+	
+	$testimony3 = "Jahz Rojas has exceed expectations at every turn. She has a good understanding of the social  media  stage  and how they all interlink. Will definately rehire her going forward.";
+	$author3 = "~ Sanjay S. Pandaram";
+	$website3 = "www.TradieClassifieds.com.au";
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -93,8 +107,65 @@
 						</div>
 					</div>
 				</div>
+			
+				<div id="testimonies">
+					<div>
+						<p>"<?php echo $testimony1; ?>"</p>
+						
+						<p class='align-right' style="right: 652px; ; bottom: 50px;"><?php echo $author1; ?></p>
+						<p class='align-right' style="right: 652px;"><?php echo $website1; ?></p>
+					</div>
+					<div>
+						<br />
+						<p>"<?php echo $testimony2; ?>"</p>
+						
+						<p class='align-right' style="right: 326px; ; bottom: 50px;"><?php echo $author2; ?></p>
+						<p class='align-right' style="right: 326px;"><?php echo $website2; ?></p>
+					</div>
+					<div>
+						<p>"<?php echo $testimony3; ?>"</p>
+						
+						<p class='align-right' style="right: 0; bottom: 50px;"><?php echo $author3; ?></p>
+						<p class='align-right' style="right: 0;"><?php echo $website3; ?></p>
+					</div>
+				</div>
 			</div>
 			
+			<div id="footer">
+				<div id="footer-content">
+					<div class="box">
+						<p class='sub-title'>Recognitions:</p>
+					</div>
+					<div class="box">
+						<p class='sub-title'>Transacts:</p>
+						<div id='transact-options'><img src='../img/transact-options.png'/></div>
+					</div>
+					<div class="box">
+						<p class='sub-title'>Connects:</p>
+						<div id="buttons">
+							<div id="fb" class="sn-icon clickable" url="https://www.facebook.com/jrojascollections"></div>
+							<div id="twitter" class="sn-icon clickable" url="https://twitter.com/imjahz"></div>
+							<div id="skype" class="sn-icon clickable" url="skype:imjahz"></div>
+							<div id="in" class="sn-icon clickable" url="http://ph.linkedin.com/in/imjahz"></div>
+						</div>
+						<div id='qrcode' class="right"><img src='../img/qrcode.png'/></div>
+						<p class='passage'>I can do all things throught Christ who strengthens me.</p>
+						<p class='from'>Philippians 4:13</p>
+					</div>
+
+					<div class="separator-3"></div>
+					<div id="footer-bottom">
+						<div class="left"><p>
+							<a href='/home'>Home</a> | 
+							<a href='#'>Services</a> | 
+							<a href='#'>Folio</a> | 
+							<a href='a'>Blog</a> | 
+							<a href='a'>Contact</a>
+							</p></div>
+						<div class="right"><p>Copyright © 2013. Jahz Rojas. All Rights Reserved</p></div>
+					</div>
+				</div>
+			</div>
         </div>
     </body>
 </html>
